91f82750a1540618a6808124a0ceb515335e2aa8
Five <kylemuse08@gmail.com>
2024-10-28 19:31:03 +0300

    Finished everything upto the 'see you then' site

--- ratejoy.js
+++ ratejoy.js
@@ -1,6 +1,49 @@
 window.onload = function () {
-  var range = document.getElementsByClassName("input")[0];
-  range.min = 0;
+  const range = document.getElementsByTagName("input")[0];
+  const rangeValue = document.getElementsByTagName("p")[0];
+  const stars = document.getElementsByClassName("stars")[0];
+  const next = document.getElementsByTagName("button")[0];
+  const link = document.getElementsByTagName("a")[0];
+  stars.style.display = "none";
+  next.style.display = "none";
+
   range.max = 10;
+  range.min = 0;
   range.value = 0;
+
+  range.addEventListener("input", () => {
+    rangeValue.innerText = range.value;
+    next.style.display = "";
+
+    if (range.value <= 2) {
+      stars.style.display = "none";
+      rangeValue.style.color = "red";
+      rangeValue.setAttribute("class", "");
+    } else if (range.value <= 3) {
+      stars.style.display = "none";
+      rangeValue.style.color = "orangered";
+      rangeValue.setAttribute("class", "");
+    } else if (range.value <= 6) {
+      stars.style.display = "none";
+      rangeValue.style.color = "rgb(255, 153, 0)";
+      rangeValue.setAttribute("class", "");
+    } else if (range.value <= 9) {
+      stars.style.display = "none";
+      rangeValue.style.color = "green";
+      rangeValue.setAttribute("class", "");
+    } else if (range.value == 10) {
+      stars.style.display = "";
+      rangeValue.style.color = "rgb(255, 72, 102)";
+      rangeValue.setAttribute("class", "ten");
+    }
+  });
+  next.addEventListener("click", () => {
+    if (range.value < 10) {
+      alert(
+        "Cant believe you actually picked a number that isn't 10. \n That's too bad coz we are still doing this. \n Damn beautiful bastard!"
+      );
+    }
+
+    link.click();
+  });
 };
